9ebe9ab5b35011ba9c11c48dcbd5d7171745a05b
divithw <divith.wijesekera@outlook.com>
2023-06-13 11:30:54 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingweightexam/HammingWeightExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingweightexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingWeightExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammingWeightExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hammingweightexam/HammingWeightExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hammingweightexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingWeightExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
